c97ed5559df1d1418c5abdcfd4c6d273b65455e9
everdebug <771441501@qq.com>
2016-04-09 18:39:58 +0800

    0409 修复欢迎界面bug --tianxy

--- app/src/main/java/graduate/txy/com/realtimebus/activity/WelcomeActivity.java
+++ app/src/main/java/graduate/txy/com/realtimebus/activity/WelcomeActivity.java
@@ -2,6 +2,7 @@ package graduate.txy.com.realtimebus.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Window;
 
 import graduate.txy.com.realtimebus.R;
@@ -16,5 +17,15 @@ public class WelcomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {// 按返回键时退出Activity的Activity特效动画
+
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            finish();
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
